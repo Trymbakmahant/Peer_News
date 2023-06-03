9a7d761fe95597ae34d5607e395332f717c8621a
Ajay krishnanand Paikra <75928188+Ajay12-p@users.noreply.github.com>
2023-06-03 16:15:00 +0530

    Update App.js

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,24 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Path from './routes/path';
+import Sidebar from './component/sidebar/sidebar';
+import { useContext } from 'react';
+import { userContext } from './context/appContext';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const ctx = useContext(userContext);
+  const sidebarFlag = ctx.sharedData.sidebarFlag;
+
+  return (<>
+  <div>
+
+
+    <Path/>
+    <div className="App bg-base-200 h-[100vh] grid grid-cols-7">
+      {sidebarFlag && 
+       (<Sidebar/>)}
+      {/*  */}
+       <h1 className="text-3xl font-bold underline col-span-6 ">
+      
+    </h1>
     </div>
+  </div>
+  
+  </>
   );
 }
 
