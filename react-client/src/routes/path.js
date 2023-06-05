5f38041c488acab2c53a53119c6d0f50bb222283
Ajay krishnanand Paikra <75928188+Ajay12-p@users.noreply.github.com>
2023-06-05 21:57:35 +0530

    Update path.js

--- react-client/src/routes/path.js
+++ react-client/src/routes/path.js
@@ -1,49 +1,22 @@
 import { Route, Routes } from "react-router-dom";
-import Navbar from "../component/Navbar/navbar";
-import LandingPage from "../component/LandingPage/Landing";
-import { useContext, useEffect, useState } from "react";
-import Sidebar from "../component/sidebar/sidebar";
-import "./path.css";
-import { userContext } from "../context/appContext";
-const Path = () => {
-  const ctx = useContext(userContext);
-  const [sidebarFlag, setSidebarFlag] = useState(true);
-  useEffect(() => {
-    setSidebarFlag(ctx.sharedData.sidebarFlag);
-  }, [ctx.sharedData.sidebarFlag]);
-  return (
-    <>
-      {" "}
-      {sidebarFlag ? (
-        <div className="grid-container">
-          <div className="grid-item item1">
-            <Navbar />
-          </div>
 
-          <div className="gird-item item2">
-            <Sidebar />
-          </div>
+import Home from "../pages/home"
+import Upload from "../pages/upload"
+import RootLayout from "../component/sidebar/RootLayout";
 
-          <div className="gird-item item5">
-            <Routes>
-              <Route exact path="/" element={<LandingPage />} />
-            </Routes>
-          </div>
-        </div>
-      ) : (
-        <div className="grid-container">
-          <div className="grid-item item1">
-            <Navbar />
-          </div>
+const Path =()=>{
 
-          <div className="gird-item item3">
-            <Routes>
-              <Route exact path="/" element={<LandingPage />} />
-            </Routes>
-          </div>
-        </div>
-      )}
-    </>
-  );
-};
+    return(<>
+    <RootLayout>
+
+    <Routes>
+        <Route exact path="/" element ={<Home/>}/>
+        <Route exact path="/upload" element ={<Upload/>}/>
+      
+    </Routes>
+    </RootLayout>
+    
+    
+    </>)
+}
 export default Path;
