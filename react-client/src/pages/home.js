54b30c077043a729e8278c129d6ac05fb36c4fe4
Ajay krishnanand Paikra <75928188+Ajay12-p@users.noreply.github.com>
2023-06-05 21:57:21 +0530

    Create home.js

---
+++ react-client/src/pages/home.js
@@ -0,0 +1,48 @@
+
+
+const Home = () => {
+
+
+    return (<>
+        {/* <div className="grid grid-cols-3 gap-4 mt-[2vh] mr-[2vh]">
+
+            <div className=" col-span-2  grid grid-rows-3 bg-base-200 gap-4 ml-4 h-[80vh]">
+
+            
+
+
+                    <div className="bg-base-100 row-span-2">
+
+                    </div>
+                    <div className="bg-base-100 gird grid-cols-3 gap-4 row-span-1">
+                        <div className="bg-base-200 col-span-1">
+
+                        </div>
+                        <div className="bg-base-200 col-span-1">
+
+                        </div>
+                        <div className="bg-base-200 col-span-1">
+
+                        </div>
+
+                    </div>
+                
+            </div>
+            <div className="bg-base-100  gap-2 ">
+
+            </div>
+
+
+        </div>
+       <div className="bg-sky-200  ml-4 h-[50vh] w-[112vh] mt-[5vh] place-content-center">
+
+       </div>   */}
+
+
+       <div>
+        sfoiwej
+       </div>
+    </>)
+}
+
+export default Home;
