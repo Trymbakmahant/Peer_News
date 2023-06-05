f8ddf2e82abc36a050be053da87c2039fc99066c
Ajay krishnanand Paikra <75928188+Ajay12-p@users.noreply.github.com>
2023-06-05 21:57:42 +0530

    Update tailwind.config.js

--- react-client/tailwind.config.js
+++ react-client/tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      extend:{
+        colors:{
+        
+        }
+      }
+    },
   },
   plugins: [require("daisyui")],
   daisyui: {
